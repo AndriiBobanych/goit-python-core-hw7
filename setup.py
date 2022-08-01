cdda14a2b58d37e046d3e3588a071f4cdb2d4bcd
Andrii Bobanych <andrii.bobanych@gmail.com>
2022-08-01 23:10:02 +0300

    commit 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='clean_folder_bai',
+    name='python_clean_folder_pip_bai',
     version='1.0.0',
     description='Package with scripts for folders handling and sorting',
     url='https://github.com/AndriiBobanych/goit-python-core-hw7',
@@ -15,9 +15,9 @@ setup(
           "Operating System :: OS Independent",
     ],
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['clean-folder=clean_folder_bai.clean:main_script']}
+    entry_points={'console_scripts': ['clean-folder=python_clean_folder_pip_bai.clean:main_script']}
     # greeting - command that shall be executed in the terminal
-    # after "=" - the path to the file where the function is located -> clean_folder_bai.clean
+    # after "=" - the path to the file where the function is located -> python_clean_folder_pip_bai.clean
     # after ":" - the function that shall be performed -> main_script
 
 )
